2c21f8bf68e7432fa7b2aabe5b3e7e94d6fa67a5
/H2O. <156546784+ixH2O@users.noreply.github.com>
2024-08-30 09:14:21 +0100

    Update light.sh

--- light.sh
+++ light.sh
@@ -30,7 +30,7 @@ echo "Installing Playit"
 curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
 sudo apt update
-sudo apt install playit
+sudo apt install playit -y
 echo "--Playit Installed--"
 clear
 
